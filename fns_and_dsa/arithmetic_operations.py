e5b597ca366a9d7d9352b76fe0497fffaf297507
YASD-404 <yasd019283@gmail.com>
2025-02-17 15:11:22 +0300

    updated code

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,16 +1,15 @@
 def perform_operation(num1, num2,operation):
-    try:
         if operation=='add':
             return num1+num2
         elif operation=='subtract':
             return num1-num2
         elif operation=='multiply':
             return num1*num2
-        else:
+        elif operation=='divide':
             if num2==0:
                 return "ZeroDivisionError"
             else:
                 return num1/num2
-    except:
-        return "Wrong data type"
+        else:
+            print("Wrong data type")
     
